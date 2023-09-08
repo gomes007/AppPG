7abd6d2caa605b89e563cbecca8813426d2bf075
Paulo Gomes <gomes7.paulo@gmail.com>
2023-09-08 09:47:59 -0300

    drawer

--- App.js
+++ App.js
@@ -1,20 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {Button, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {createDrawerNavigator, DrawerContentScrollView, DrawerItem} from '@react-navigation/drawer';
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
+import BookStackNavigator from './stack-navigators/BookStackNavigator';
+import ContactStackNavigator from './stack-navigators/ContactStackNavigator';
+import LocationsStackNavigator from "./stack-navigators/LocationsStackNavigator";
+import MyRewardsStackNavigator from "./stack-navigators/MyRewardsStackNavigator";
+
+const Drawer = createDrawerNavigator();
+
+const HomeScreen = () => {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Home Screen</Text>
+        </View>
+    );
+};
+
+const AnotherScreen = () => {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Another Screen</Text>
+        </View>
+    );
+};
+
+const DrawerContent = (props) => {
+    const { navigation } = props;
+    const navigateToScreen = (route) => {
+        navigation.navigate(route);
+        navigation.closeDrawer();
+    };
+
+    return (
+        <DrawerContentScrollView {...props}>
+            <DrawerItem label="Home" onPress={() => navigateToScreen('Home')} />
+            <DrawerItem label="MyRewards" onPress={() => navigateToScreen('MyReward')} />
+            <DrawerItem label="Book" onPress={() => navigateToScreen('Books')} />
+        </DrawerContentScrollView>
+    );
+};
+
+const BottomTabs = () => {
+    const navigation = useNavigation();
+
+    return (
+        <View style={styles.bottomTabs}>
+            <Button title="Home" onPress={() => navigation.navigate('Home')} />
+            <Button title="Location" onPress={() => navigation.navigate('Location')} />
+            <Button title="Contact" onPress={() => navigation.navigate('Contact')} />
+        </View>
+    );
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <NavigationContainer>
+                <View style={{ flex: 1 }}>
+                    <Drawer.Navigator initialRouteName="Home" drawerContent={props => <DrawerContent {...props} />}>
+                        <Drawer.Screen name="Home" component={HomeScreen} />
+                        <Drawer.Screen name="Another" component={AnotherScreen} />
+                        <Drawer.Screen name="Books" component={BookStackNavigator} />
+                        <Drawer.Screen name="Contact" component={ContactStackNavigator} />
+                        <Drawer.Screen name="Location" component={LocationsStackNavigator} />
+                        <Drawer.Screen name="MyReward" component={MyRewardsStackNavigator} />
+                    </Drawer.Navigator>
+                    <BottomTabs />
+                </View>
+            </NavigationContainer>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    bottomTabs: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        backgroundColor: 'grey',
+        padding: 10,
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+    },
 });
---
+++ navigation/DrawerNavigator.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import {StyleSheet, Text, TouchableOpacity} from 'react-native'
+import {createDrawerNavigator, DrawerContentScrollView, DrawerItem} from '@react-navigation/drawer'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+import MyRewardsStackNavigator from '../stack-navigators/MyRewardsStackNavigator'
+import LocationsStackNavigator from '../stack-navigators/LocationsStackNavigator'
+
+const Drawer = createDrawerNavigator()
+
+const CustomDrawerContent = (props) => {
+    return (
+        <DrawerContentScrollView {...props}>
+            {
+                Object.entries(props.descriptors).map(([key, descriptor], index) => {
+                    const focused = index === props.state.index
+                    return (
+                        <DrawerItem
+                            key={key}
+                            label={() => (
+                                <Text style={focused ? styles.drawerLabelFocused : styles.drawerLabel}>
+                                    {descriptor.options.title}
+                                </Text>
+                            )}
+                            onPress={() => descriptor.navigation.navigate(descriptor.route.name)}
+                            style={[styles.drawerItem, focused ? styles.drawerItemFocused : null]}
+                        />
+                    )
+                })
+            }
+        </DrawerContentScrollView>
+    )
+}
+
+const DrawerNavigator = ({ initialRoute }) => {
+    return (
+        <Drawer.Navigator
+            screenOptions={({ navigation }) => ({
+                headerStyle: {
+                    backgroundColor: '#551E18',
+                    height: 50,
+                },
+                headerLeft: () => (
+                    <TouchableOpacity onPress={() => navigation.toggleDrawer()} style={styles.headerLeft}>
+                        <Icon name="bars" size={20} color="#fff" />
+                    </TouchableOpacity>
+                ),
+            })}
+            drawerContent={(props) => <CustomDrawerContent {...props} />}
+        >
+
+            <Drawer.Screen name="MyRewardsStack" component={MyRewardsStackNavigator} options={{
+                title: 'My Rewards',
+                headerTitle: () => <Text style={styles.headerTitle}>My Rewards</Text>,
+            }}/>
+            <Drawer.Screen name="LocationsStack" component={LocationsStackNavigator} options={{
+                title: 'Locations',
+                headerTitle: () => <Text style={styles.headerTitle}>Our Locations</Text>,
+            }}/>
+        </Drawer.Navigator>
+    )
+}
+
+const styles = StyleSheet.create({
+    headerLeft: {
+        marginLeft: 15,
+    },
+    headerTitle: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: '500',
+    },
+    headerRight: {
+        marginRight: 15,
+    },
+    // drawer content
+    drawerLabel: {
+        fontSize: 14,
+    },
+    drawerLabelFocused: {
+        fontSize: 14,
+        color: '#551E18',
+        fontWeight: '500',
+    },
+    drawerItem: {
+        height: 50,
+        justifyContent: 'center'
+    },
+    drawerItemFocused: {
+        backgroundColor: '#ba9490',
+    },
+})
+
+export default DrawerNavigator
